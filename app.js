b2aa151b517595f622478da2f22357ff9a4fa68a
Kenneth Van Der Strieckt <49682872+KennethVDS@users.noreply.github.com>
2019-06-26 16:08:21 +0200

    add personal styling

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const cards = document.querySelectorAll('.memory-card');
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.add('flip');
+
     if(!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
@@ -22,9 +30,22 @@ function disableCards() {
 }
     
 function unflipCards() {  
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
-    }, 1500);
-};
+
+        lockBoard = false;
+    }, 1100);
+}
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null]
+}
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    })
+})();
 cards.forEach(card => card.addEventListener('click', flipCard));
